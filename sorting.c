b24cd931623c903e64d107d4c354856b2b6236f7
Bharath Krishna <phantomkrisx@gmail.com>
2025-09-24 19:37:02 +0530

    added more sort functions

--- sorting.c
+++ sorting.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+void swap(int *x,int* y){
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
 void bubble_sort(int A[],int n){
     for(int pass=1;pass<=n-1;pass++){
         for(int i=0;i<n-pass-1;i++){
             if(A[i]>A[i+1]){
-                int temp=A[i+1];
-                A[i+1]=A[i];
-                A[i]=temp;
+                swap(&A[i],&A[i+1]);
             }
         }
+    }
+}
+void selection_sort(int A[],int n){
+    for(int i=0;i<n-1;i++){
+        int mindex=i;
+        for(int j=i+1;j<n;j++){
+            if(A[j]<A[mindex]){
+                mindex=j;
+            }
+        }
+        if(mindex!=i)
+        swap(&A[i],&A[mindex]);
     }
 }
 void insertion_sort(int A[],int n){
@@ -22,6 +37,67 @@ void insertion_sort(int A[],int n){
         A[j+1]=key;
     }
 }
+int partition(int A[],int low,int high){
+    int pivot=A[high];
+    int i=low-1;
+    int j=low;
+    for(;j<high;j++){
+        if(A[j]<=pivot){
+            i++;
+            swap(&A[i],&A[j]);
+        }
+    }
+    swap(&A[i+1],&A[high]);
+    return i+1;
+}
+void quick_sort(int A[],int low,int high){
+    if(low<high){
+        int pi=partition(A,low,high);
+        quick_sort(A,low,pi-1);
+        quick_sort(A,pi+1,high);
+    }
+}
+void merge(int A[],int l,int m,int r){
+    int n1=m-l+1;
+    int n2=r-m;
+    int L[n1],R[n2];
+    for(int i=0;i<n1;i++){
+        L[i]=A[l+i];
+    }
+    for(int j=0;j<n2;j++){
+        R[j]=A[m+1+j];
+    }
+    int i=0,j=0,k=l;
+    while(i<n1&&j<n2){
+        if(L[i]<=R[j]){
+            A[k]=L[i];
+            i++;
+        }
+        else{
+            A[k]=R[j];
+            j++;
+        }
+        k++;
+    }
+     while (i < n1) {
+        A[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        A[k] = R[j];
+        j++;
+        k++;
+    }
+}
+void merge_sort(int A[],int l,int r){
+    if(l<r){
+        int m=(l+r)/2;
+        merge_sort(A,l,m);
+        merge_sort(A,m+1,r);
+        merge(A,l,m,r);
+    }
+}
 int main(){
     int *A,n,choice;
     printf("Enter number of elements: ");
@@ -31,7 +107,7 @@ int main(){
     for(int i=0;i<n;i++){
         scanf("%d",&A[i]);
     }
-    printf("Choose sorting method:\n1. Bubble Sort\n2. Insertion Sort\n");
+    printf("Choose sorting method:\n1. Bubble Sort\n2. Insertion Sort\n 3. Quick Sort\n 4. Selection Sort\n 5. Merge Sort\n");
     scanf("%d",&choice);
     switch(choice){
         case 1:
@@ -39,6 +115,15 @@ int main(){
             break;
         case 2:
             insertion_sort(A,n);
+            break;
+        case 3:
+            quick_sort(A,0,n-1);
+            break;
+        case 4:
+            selection_sort(A,n);
+            break;
+        case 5:
+            merge_sort(A,0,n-1);
             break;
         default:
             printf("Invalid choice\n");
